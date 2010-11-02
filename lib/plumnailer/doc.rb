6f3502b2ff09897d61ecfd0234226c40e03acf73
Matthew M. Boedicker <matthewm@boedicker.org>
2010-11-02 01:23:30 -0400

    handle invalid uri

--- lib/plumnailer/doc.rb
+++ lib/plumnailer/doc.rb
@@ -12,10 +12,18 @@ module Plumnailer
 
     # Return a list of the absolute urls of all imgs in the document.
     def img_abs_urls(base_url=nil)
-      img_srcs.map do |i|
-        u = URI(i)
-        u.is_a?(URI::HTTP) ? u : URI.join(base_url || source_url, i)
+      result = []
+
+      img_srcs.each do |i|
+        begin
+          u = URI(i)
+        rescue URI::InvalidURIError
+          next
+        end
+        result.push(u.is_a?(URI::HTTP) ? u : URI.join(base_url || source_url, i))
       end
+
+      result
     end
 
     attr_accessor :source_url
