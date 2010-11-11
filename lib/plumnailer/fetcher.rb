5f75875040cd450813d6cd475630ace1f09ce1de
Matthew M. Boedicker <matthewm@boedicker.org>
2010-11-10 19:37:35 -0500

    add  user agent

--- lib/plumnailer/fetcher.rb
+++ lib/plumnailer/fetcher.rb
@@ -5,10 +5,14 @@ module Plumnailer
   # Fetch the contents of a url.
   class Fetcher
 
+    UserAgent = 'plumnailer (http://github.com/mmb/plumnailer)'
+
     # Fetch the contents of a url.
     def fetch(url)
       uri = url.is_a?(URI) ? url : URI(url)
-      open(uri) { |f| f.read } if uri.is_a?(URI::HTTP)
+      if uri.is_a?(URI::HTTP)
+        open(uri, 'User-Agent' => UserAgent) { |f| f.read }
+      end
     end
 
   end
