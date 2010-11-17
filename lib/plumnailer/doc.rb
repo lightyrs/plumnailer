b839fb9aa6690bd9e911f2fa8ad0b21e643efb91
Matthew M. Boedicker <matthewm@boedicker.org>
2010-11-17 01:09:13 -0500

    check for base tag in head when building absolute urls

--- lib/plumnailer/doc.rb
+++ lib/plumnailer/doc.rb
@@ -5,6 +5,12 @@ module Plumnailer
   # Nokogiri::HTML:Document mixin.
   module Doc
 
+    # Get the href attribute of the base tag from the head of the document.
+    def doc_base_href
+      base = at('//head/base')
+      base['href']  if base
+    end
+
     # Return a list of the src attributes of all img tags.
     def img_srcs
       search('//img').map { |x| x['src'] }.compact
@@ -20,7 +26,12 @@ module Plumnailer
         rescue URI::InvalidURIError
           next
         end
-        result.push(u.is_a?(URI::HTTP) ? u : URI.join(base_url || source_url, i))
+
+        result <<  if u.is_a?(URI::HTTP)
+          u
+        else
+          URI.join(base_url || doc_base_href || source_url, i)
+        end
       end
 
       result
